9e1e759ef32e41255dd6d0f9f0a8064a4e3b5524
Ariostgx <609557547@qq.com>
2018-12-05 16:27:12 +0800

    delete unsed files

--- join_test.sh
+++
@@ -1,4 +0,0 @@
-python joint_tune_eval.py --sources 'viper' --target 'viper' --epochs 150 --method 'viper'
-python joint_tune_eval.py --sources 'ilids' --target 'ilids' --epochs 150 --method 'ilids'
-python joint_tune_eval.py --sources '3dpes' --target '3dpes' --epochs 150 --method '3dpes'
-
